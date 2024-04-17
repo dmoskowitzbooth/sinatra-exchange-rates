b1f77d1e4828c2bd47ee25cb37ea3de30b1e5a7a
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-17 19:47:15 +0000

    3

--- app.rb
+++ app.rb
@@ -16,5 +16,24 @@ end
 
 get("/:a_curr") do
   @cfrom=params.fetch("a_curr").to_s
+
+  curr_key=ENV["EXCHANGE_RATE_KEY"]
+  curr_list="https://api.exchangerate.host/list?access_key=#{curr_key}"
+  curr=HTTP.get(curr_list)
+  curr_parsed=JSON.parse(curr)
+  @curr_results=curr_parsed["currencies"].keys
+
+ erb(:convert)
+end
+
+get("/:a_curr/:cfrom") do
+  @cfrom=params.fetch("a_curr").to_s
+
+  curr_key=ENV["EXCHANGE_RATE_KEY"]
+  curr_list="https://api.exchangerate.host/list?access_key=#{curr_key}"
+  curr=HTTP.get(curr_list)
+  curr_parsed=JSON.parse(curr)
+  @curr_results=curr_parsed["currencies"].keys
+  
  erb(:convert)
 end
